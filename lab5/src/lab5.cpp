8ad4091b6c0672dd1b88903c54eecc2c5630a1ee
geethasklu <geetha.s@cmrit.ac.in>
2016-03-29 11:30:20 +0530

    frmmain_index

--- lab5/src/lab5.cpp
+++ lab5/src/lab5.cpp
@@ -10,6 +10,15 @@
 using namespace std;
 
 int main() {
+	string key;
+	cout<<"key to search";
+Student s("gita","12");
+RF  f("data.txt");
+f.write(s.pack());
+f.read(key);
+f.delet(key);
+}
+
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
