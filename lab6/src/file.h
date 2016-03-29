2df741f597de5d93f6111b9459e3656387fa0642
geethasklu <geetha.s@cmrit.ac.in>
2016-03-29 12:18:50 +0530

    Create file.h

---
+++ lab6/src/file.h
@@ -0,0 +1,13 @@
+class file{
+    string name;
+    string usn;
+    string branch;
+    int sem;
+    string buffer;
+public:
+    int pos;
+    void write(string);
+    void unpack(int);
+    //int search(string);
+    void del(int);
+};
