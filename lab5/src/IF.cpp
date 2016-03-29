7d12ece257339d564eb41abab60cb21d701f8fb5
geethasklu <geetha.s@cmrit.ac.in>
2016-03-29 11:42:12 +0530

    Create IF.cpp

---
+++ lab5/src/IF.cpp
@@ -0,0 +1,16 @@
+IF::IF(int maxkeys)
+{
+  max=maxkeys;
+  key[]=keys[];
+  addr-i=addr;
+ }
+ IF::insert(string key,int addr)
+  {
+    for(int i=0;i<max;i++)
+    {
+      key[i+1]=keys[i];
+      addr-i=addr;
+      nokeys++;
+    }
+  }  
+  
